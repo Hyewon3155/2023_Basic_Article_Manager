18efe19e8ff6909f073a40541b3403c6e48b4f57
userName <useremail>
2023-02-15 14:52:34 +0900

    게시물 상세보기 기능 구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -53,7 +53,34 @@ public class Main {
 					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
 				
-			} else {
+			}  else if (cmd.startsWith("article detail ")) {
+				
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+				
+				Article foundArticle = null;
+				
+				for(int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					
+					if(article.id == id) {
+						foundArticle = article;
+						System.out.printf("번호: %d\n", foundArticle.id);
+						System.out.printf("날짜: %s\n", foundArticle.id);
+						System.out.printf("제목: %s\n", foundArticle.title);
+						System.out.printf("내용: %s\n", foundArticle.body);
+						break;
+					}
+				}
+				if(foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+                }
+				
+				
+				
+				
+			}else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
