f6281ff02d57aa07c208f56aafa79b3e2486f968
userName <useremail>
2023-02-16 13:45:19 +0900

    게시물 삭제 기능 구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -3,6 +3,7 @@ package com.koreaIT.java.BAM;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Iterator;
 
 public class Main {
 	public static void main(String[] args) {
@@ -53,7 +54,7 @@ public class Main {
 					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
 				
-			}  else if (cmd.startsWith("article detail ")) {
+			} else if (cmd.startsWith("article detail ")) {
 				
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
@@ -65,21 +66,50 @@ public class Main {
 					
 					if(article.id == id) {
 						foundArticle = article;
-						System.out.printf("번호: %d\n", foundArticle.id);
-						System.out.printf("날짜: %s\n", foundArticle.id);
-						System.out.printf("제목: %s\n", foundArticle.title);
-						System.out.printf("내용: %s\n", foundArticle.body);
 						break;
 					}
 				}
+				
 				if(foundArticle == null) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 					continue;
-                }
+				}
 				
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", "2023-02-15 13:13:13");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
 				
+			} else if (cmd.startsWith("article delete")){
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
 				
+				Article foundArticle = null;
 				
+				for(int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					
+					if(article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if(foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				
+				Iterator<Article> iterator = articles.iterator();
+
+				while(iterator.hasNext()) {
+				  Article str = iterator.next();
+				  
+				  if (str.id == id) {
+					  articles.remove(id);
+				  }
+					  
+				}
 			}else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
